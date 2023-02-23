9f63a3f403c343b567ee34d1bad0fcf4fcf4ce4f
Marvjex <jexmarvin@gmail.com>
2023-02-23 13:10:53 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
  * main - A c program that prints a line to the standard error using print f
  * Return 1 (success)
+ * main - Enry point
  */
- int main(void)
+int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
