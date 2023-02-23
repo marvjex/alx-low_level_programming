e451da187f7b83715912e971fca5152925366d3b
Marvjex <jexmarvin@gmail.com>
2023-02-23 13:07:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that prints a line to the standard error using print f
  * Return 1 (success)
  */
-int main(void)
+ int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
