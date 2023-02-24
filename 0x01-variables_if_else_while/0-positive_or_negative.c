432c583625694eed0869b7966a19ba04463fdee0
Marvjex <jexmarvin@gmail.com>
2023-02-23 22:28:10 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- * Return: Always 0 success
+ * Main - Entry point
+ * Description 'checks the number if its positive or negative
+ * Return: Always 0
  */
 int main(void)
 {
