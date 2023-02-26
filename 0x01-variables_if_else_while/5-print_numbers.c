a149ab23557659d4ae932a3d59f4d20420cd3066
Marvjex <jexmarvin@gmail.com>
2023-02-26 12:55:47 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-/* betty documentation type */
 /**
- * A program prints single digit numbers o base 10
- * main - This is the entry point of the program
- * Return: 0 always (succedded)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
+int a;
 for (a = 0; a <= 9; a++)
-	printf("%d", a);
+printf("%d", a);
 putchar('\n');
 return (0);
 }
