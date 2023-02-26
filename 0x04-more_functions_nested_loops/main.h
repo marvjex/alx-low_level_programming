e09b2b742617fbaa0d6e406ae503db48d1263539
Marvjex <jexmarvin@gmail.com>
2023-02-25 18:46:02 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,15 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+#include <stdio.h>
+void print_triangle(int size);
+#include <stdio.h>
+void print_number(int n);
 #endif 
