1183ae7db8a2a07ded90664b113e44639b198b07
root <jexmarvin@gmail.com>
2024-03-21 18:07:24 -0700

    printing single lists data thus returning the number of data

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 /**
-  * print_listint - prints all the elements of a linked list
-  * @h: linked list of type listint_t to print
+  * print_listint - prints the int data in a singly linked list
+  * @h: the head pointer to the list
   * Return: number of nodes
   */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+size_t count;
 
-while (h)
+if (h == NULL)
+	return (0);
+for (count = 0; h != NULL; count++)
 {
 printf("%d\n", h->n);
-num++;
 h = h->next;
 }
-return (num);
+return (count);
 }
