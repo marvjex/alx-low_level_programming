308a8bbe66c8c04cc9bb9c57bef15d95460ac81a
root <jexmarvin@gmail.com>
2024-03-22 07:06:58 -0700

    count the number of elements in a singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 /**
-  * print_listint - prints all the elements of a linked list
-  * @h: linked list of type listint_t to print
-  *
-  * Return: number of nodes
+  * listint_len - count the number of element in a list
+  * @h: head pointer
+  * Return: number of element in the list
   */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-size_t num = 0;
-
-while (h)
+size_t count;
+if (h == NULL)
+	return (0);
+for (count = 0; h != NULL; count++)
 {
-printf("%d\n", h->n);
-num++;
 h = h->next;
 }
-
-return (num);
+return (count);
 }
 
